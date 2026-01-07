97fdbbeef6e4bb2b61a87afb97d78b56c5738ed5
Cícero Julião da Silva Junior <juliaojunior@gmail.com>
2026-01-07 11:55:09 -0300

    fix: match android package id with firebase and update capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.todah.app',
+  appId: 'com.todahs.app',
   appName: 'ToDAH',
   webDir: 'public',
   server: {
